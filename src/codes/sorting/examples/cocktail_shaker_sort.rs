f80e9dbe06f7c38a9ad3ab985075545ac78eb6e3
jalaxy33 <jalaxy1996@outlook.com>
2025-09-03 10:20:58 +0800

    update cocktail sort & comb sort

--- src/codes/sorting/examples/cocktail_shaker_sort.rs
+++ src/codes/sorting/examples/cocktail_shaker_sort.rs
@@ -1,19 +1,32 @@
-# 鸡尾酒排序
+//! 鸡尾酒排序
 
-[代码实现](../../codes/sorting/examples/cocktail_shaker_sort.rs)
+/**
+鸡尾酒排序可以理解成冒泡排序的双向版本。先从左到右遍历一遍，再从右到左遍历一遍，直到没有元素需要交换为止。
 
-```rust
-pub fn cocktail_shaker_sort<T: Ord>(arr: &mut [T]) {
+### 分析
+
+从左到右遍历时，最大的元素会被交换到最右边；从右到左遍历时，最小的元素会被交换到最左边。
+每次双向遍历后，未排序部分的范围都会缩小。
+
+时间复杂度：O(n^2)，空间复杂度：O(1)。
+
+### 优化
+
+引入一个变量标记当前循环是否发生元素交换，如果没有发生交换，说明数组已经有序，可以提前结束排序。
+ */
+fn cocktail_shaker_sort<T: Ord>(arr: &mut [T]) {
     let len = arr.len();
-
+    
     if len == 0 {
         return;
     }
 
+    let mut bottom = 0;
+    let mut top = len - 1;
+
     loop {
         let mut swapped = false;
-
-        for i in 0..(len - 1).clamp(0, len) {
+        for i in bottom..top {
             if arr[i] > arr[i + 1] {
                 arr.swap(i, i + 1);
                 swapped = true;
@@ -24,20 +37,22 @@ pub fn cocktail_shaker_sort<T: Ord>(arr: &mut [T]) {
             break;
         }
 
-        swapped = false;
-
-        for i in (0..(len - 1).clamp(0, len)).rev() {
-            if arr[i] > arr[i + 1] {
-                arr.swap(i, i + 1);
+        top -= 1;
+        for j in ((bottom + 1)..=top).rev() {
+            if arr[j] < arr[j - 1] {
+                arr.swap(j, j - 1);
                 swapped = true;
             }
         }
+        bottom += 1;
 
         if !swapped {
             break;
         }
     }
 }
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -71,4 +86,3 @@ mod tests {
         assert_eq!(arr, vec![1, 2, 3, 4, 5, 6]);
     }
 }
-```
--- src/codes/sorting/examples/comb_sort.rs
+++ src/codes/sorting/examples/comb_sort.rs
@@ -1,20 +1,31 @@
-# 梳排序
+//! 梳排序
 
-[代码实现](../../codes/sorting/examples/comb_sort.rs)
+/**
+梳排序是一种改进的冒泡排序，开始时会使用一个较大的间隔对元素进行比较和交换，
+随着排序的进行，逐渐减小间隔，最终达到相邻元素的比较。
 
+### 分析
 
-```rust
-pub fn comb_sort<T: Ord>(arr: &mut [T]) {
+开始时间隔通常设为数组长度，随后以一个固定的收缩率逐渐减小（通常取 1.3），直到间隔为1。
+因为编程语言中乘法比除法快，故会取递减率倒数 (0.8) 与间距相乘。
+
+复杂度：
+- 时间复杂度：本质上还是冒泡排序，O(n^2)；
+- 空间复杂度：O(1)。
+
+ */
+fn comb_sort<T: Ord>(arr: &mut [T]) {
     let mut gap = arr.len();
-    let shrink = 1.3;
+    let shrink = 0.8;
     let mut sorted = false;
 
     while !sorted {
-        gap = (gap as f32 / shrink).floor() as usize;
+        gap = (gap as f32 * shrink) as usize;
         if gap <= 1 {
             gap = 1;
             sorted = true;
         }
+
         for i in 0..arr.len() - gap {
             let j = i + gap;
             if arr[i] > arr[j] {
@@ -24,6 +35,9 @@ pub fn comb_sort<T: Ord>(arr: &mut [T]) {
         }
     }
 }
+
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -49,4 +63,3 @@ mod tests {
         }
     }
 }
-```
